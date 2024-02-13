0bf112bcea503e5639e70a232d3751a25362b3d8
Dominique Makowski <dom.mak19@gmail.com>
2024-02-13 18:11:39 +0000

    update analysis

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 A reanalysis of Murphy, J., Brewer, R., Plans, D., Khalsa, S. S., Catmur, C., & Bird, G. (2020). [Testing the independence of self-reported interoceptive accuracy and attention](https://journals.sagepub.com/doi/full/10.1177/1747021819879826). Quarterly Journal of Experimental Psychology, 73(1), 115-133. https://doi.org/10.31234/osf.io/fqgu4
 
 
-
-- [**Preliminary manuscript**](https://github.com/DominiqueMakowski/InteroceptiveAccuracyScale/blob/main/paper/1_Initial/manuscript.pdf)
-- [**Results**](https://dominiquemakowski.github.io/InteroceptiveAccuracyScale/analysis.html)
+- [**Results (old)**](https://realitybending.github.io/InteroceptionIAS/analysis/old/analysis.html)
 
 
 ![](figures/Figure1.png)
---
+++ paper/1_Initial/_extensions/wjschne/apaquarto/latexnoindent.lua
@@ -0,0 +1,33 @@
+if FORMAT:match 'latex' then
+  
+  local indenter = '\\setlength\\parindent{0.5in}'
+  
+  Meta = function(m)
+    if pandoc.utils.stringify(m.documentmode) == 'jou' then 
+      indenter = '\\setlength\\parindent{0.15in}'
+    end
+
+  end
+  
+  Div = function (div)
+
+    if div.classes:includes 'NoIndent' then
+        div.content = div.content:walk {
+          Para = function(p)
+            p.content:insert(1, pandoc.RawInline("latex", "\\noindent "))
+            return p
+          end
+        }
+        --pandoc.RawBlock('latex', '\\setlength\\parindent{0in}'),
+        --div,
+        --pandoc.RawBlock('latex', indenter)
+      return(div)
+    end
+  end
+  
+  return {
+  { Meta = Meta },
+  { Div = Div }
+}
+end
+
